2489fb1dc563f271d6185fbeabd88ae42a51d396
Lucas Picinini <137008535+lucaspicinini@users.noreply.github.com>
2024-01-23 14:53:20 -0300

    correcting social card preview

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      image: 'img/social-card.jpg',
+      image: 'img/social-card.png',
       navbar: {
         title: 'Home',
         logo: {
